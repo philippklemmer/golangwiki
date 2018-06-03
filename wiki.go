dafd48e9e8f5217abdfec8ff25f92c334b6e39f8
K3rne1.Pan1c <philippklemmer.web@gmail.com>
2018-06-04 00:01:59 +0200

    added basic logic for wiki, added basic layout structure

---
+++ wiki.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"log"
+	"regexp"
+)
+
+var templates = template.Must(template.ParseGlob("tmpl/*"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile("data/" + filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile("data/" + filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+func redirectRootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "view/Frontpage", http.StatusFound)
+}
+
+// The returned func is called a closure because it encloses values defined outside of it. 
+// In this case, the variable fn(the single argument to makeHandler) is enclosed by the closure. 
+// The variable fn will be one of our view handlers.
+// The closure returned be makeHandler is a function that takes an
+// http.ResponseWriter and http.Request (in other words, an http.HandlerFunc)
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//Here we will extract the page title from the Request,
+		// and call the provided handler 'fn'
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w,r,m[2])
+	}
+}
+
+func main() {
+	files, _ := ioutil.ReadDir("./")
+	for _, f := range files {
+		log.Print(f.Name())
+	}
+	http.HandleFunc("/", redirectRootHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
